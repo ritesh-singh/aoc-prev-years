8d7aea366e9b4a2e5e1299518ede19453a2509d1
Ritesh.Singh <riteshdvps10@gmail.com>
2024-01-05 17:53:34 +0530

    Day 1 y_2015

--- src/y_2015/day01/Day01.kt
+++ src/y_2015/day01/Day01.kt
@@ -3,13 +3,13 @@ package y_2015.day01
 import y_2015.readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input[0].fold(0) { acc, c -> if (c == '(') acc + 1 else acc - 1 }
 
-    val testInput = readInput("y_2015/day01","Day01_test")
+    fun part2(input: List<String>): Int =
+        input[0].runningFold(0) { acc, c -> if (c == '(') acc + 1 else acc - 1 }.indexOfFirst { it == -1 }
+
+    val input = readInput("y_2015/day01", "Day01")
+    println(part1(input))
+    println(part2(input))
 }
