10157cdc25ef89b03407610de35d6e7300f51f76
Ritesh Kumar Singh <Ritesh.Singh@target.com>
2024-01-05 15:04:26 +0530

    prev year format

--- src/y_2015/Utils.kt
+++ src/y_2015/Utils.kt
@@ -1,3 +1,5 @@
+package y_2015
+
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -6,7 +8,9 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(yearDay: String, name: String):List<String>  {
+    return Path("src/$yearDay/$name.txt").readLines()
+}
 
 /**
  * Converts string to md5 hash.
@@ -15,7 +19,3 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
     .toString(16)
     .padStart(32, '0')
 
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
