e507f2cad67ec84353f06a835162c233e56b80a4
Ritesh.Singh <riteshdvps10@gmail.com>
2024-01-05 15:04:26 +0530

    prev year format

---
+++ src/y_2015/day01/Day01.kt
@@ -0,0 +1,15 @@
+package y_2015.day01
+
+import y_2015.readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("y_2015/day01","Day01_test")
+}
