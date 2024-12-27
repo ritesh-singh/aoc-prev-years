72d9ab46d66aa4a0a74a83f2275bfe264438c418
Ritesh.Singh <riteshdvps10@gmail.com>
2024-12-27 07:38:33 +0530

    refactor

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,7 +1,6 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.io.path.readText
 
 /**
  * Reads lines from the given input txt file.
@@ -10,10 +9,51 @@ fun readInput(yearDay: String, name: String):List<String>  {
     return Path("src/$yearDay/$name.txt").readLines()
 }
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun readText(name: String) = Path("src/$name.txt").readText().trim()
+
+fun Any?.println() = println(this)
+
+fun printGrid(grid: Any) {
+    when (grid) {
+        is List<*> -> {
+            if (grid.isEmpty()) throw IllegalStateException("Can't print empty grid")
+            when(val fE = grid.first()){
+                is String -> {
+                    for (row in grid) println(row)
+                }
+                is List<*> -> {
+                    for (row in grid)
+                        println((row as List<*>).joinToString(" ") { it.toString() })
+                }
+                is CharArray -> {
+                    for (row in grid) println(row.toString())
+                }
+                else -> throw IllegalArgumentException("Unsupported grid type")
+            }
+        }
+    }
+}
+
+data class Coord(val x: Int, val y: Int) {
+    override fun toString(): String {
+        return "($x, $y)"
+    }
+}
+
+fun Coord.inRange(grid: List<String>) = x in grid.indices && y in grid[0].indices
+
+fun Coord.up() = Coord(x - 1, y)
+fun Coord.down() = Coord(x + 1, y)
+fun Coord.left() = Coord(x, y - 1)
+fun Coord.right() = Coord(x, y + 1)
+fun Coord.all() = listOf(up(),down(),left(),right())
+
+enum class Direction {
+    E,W,N,S
+}
+
+enum class Env {
+    TEST, REAL
+}
+
 
--- src/y_2015/day01/Day01.kt
+++ src/y_2015/day01/Day01.kt
@@ -1,9 +1,12 @@
 package y_2015.day01
 
-private class Day01(){
+import Env
+import readInput
 
+private class Day01(env:Env){
+    private val input = readInput("y_2015/day01","Day01_test")
 }
 
 fun main() {
-
+    Day01(env = Env.TEST)
 }
