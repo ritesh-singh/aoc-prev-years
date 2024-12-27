10370b0849b9108f0c126c330c32994c6abbff45
Ritesh.Singh <riteshdvps10@gmail.com>
2024-12-27 13:05:42 +0530

    Day 2

--- src/y_2015/day02/Day02.kt
+++ src/y_2015/day02/Day02.kt
@@ -1,9 +1,25 @@
 package y_2015.day02
 
-private class Day02(){
+import Env
+import println
+import readInput
+
+private class Day02(env: Env) {
+    private val input = readInput("y_2015/day02", if (env == Env.TEST) "Day02_test" else "Day02")
+
+    fun totalSqFtWrappingPaper() = input.sumOf {
+        val (l, w, h) = it.split("x").map { it.trim().toLong() }
+        (2 * l * w) + (2 * w * h) + (2 * h * l) + listOf(l * w, w * h, h * l).min()
+    }
+
+    fun totalFtRibbon() = input.sumOf {
+        val (l, w, h) = it.split("x").map { it.trim().toLong() }
+        (l * w * h) + listOf(l, w, h).sorted().take(2).map { it * 2 }.sum()
+    }
 
 }
 
 fun main() {
-
+    Day02(Env.REAL).totalSqFtWrappingPaper().println()
+    Day02(Env.REAL).totalFtRibbon().println()
 }
